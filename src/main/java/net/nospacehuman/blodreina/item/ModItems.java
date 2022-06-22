01087e76dea558b01b6dfda59e9edc68a4130866
lackingcats <jackson.marsden6@gmail.com>
2022-06-22 17:46:20 +1000

    configured the "food"

--- src/main/java/net/nospacehuman/blodreina/item/ModItems.java
+++ src/main/java/net/nospacehuman/blodreina/item/ModItems.java
@@ -17,7 +17,7 @@ public class ModItems {
             new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE).maxCount(64)));
     public static final Item SHARD_OF_TANZANITE = registerItem("shard_of_tanzanite",
             new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE).maxCount(64).food(
-                    new FoodComponent.Builder().statusEffect(new StatusEffectInstance(StatusEffects.LEVITATION,20*120, 255, false, false, false), 1f).snack().build())));
+                    new FoodComponent.Builder().statusEffect(new StatusEffectInstance(StatusEffects.LEVITATION,20*300, 255, false, false, false), 1f).snack().build())));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(NouBlodreinaNowe.MOD_ID, name), item);
